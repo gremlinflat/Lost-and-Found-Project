fd49a175fef0d4b12c0a95063f92c7d62eca2123
gremlinflat_ <fahri.119140205@student.itera.ac.id>
2021-11-23 22:52:21 +0700

    debug

--- lostandfoundweb/settings.py
+++ lostandfoundweb/settings.py
@@ -151,3 +151,15 @@ LOGIN_URL = 'user-login'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
